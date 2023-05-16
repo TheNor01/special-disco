2fcdcc5a4853d4cce8e7ee6fc1b5c90d17dae5b5
Aldo Fiorito <aldo.fiorito97@gmail.com>
2023-05-16 20:59:40 +0200

    First point

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -3,18 +3,22 @@ import weka.attributeSelection.AttributeSelection;
 import weka.attributeSelection.Ranker;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Add;
+import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.instance.RemovePercentage;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.StringToNominal;
 import weka.filters.supervised.instance.SMOTE;
 import weka.attributeSelection.InfoGainAttributeEval;
 
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -23,10 +27,160 @@ import java.util.Random;
  */
 public class App
 {
+
+    private static void FormatDataset(ArrayList<String> sources) throws Exception{
+
+        ArrayList<DataSource> data = new ArrayList<>();
+        ArrayList<Instances> inputInstances = new ArrayList<>();
+        ArrayList<Instances> outputInstances = new ArrayList<>();
+
+        //create DataSources
+        for(String s : sources){
+            data.add(new DataSource(s));
+        }
+
+        //create Instances
+        for(DataSource ds : data){
+            inputInstances.add(ds.getDataSet());
+        }
+
+        //rename and SET class
+
+        for(Instances is : inputInstances){
+            is.renameAttribute(0,"attr1");
+            is.renameAttribute(1,"attr2");
+            is.renameAttribute(2,"attr3");
+            is.renameAttribute(3,"attr4");
+            is.renameAttribute(4,"class");
+            if (is.classIndex() == -1) is.setClassIndex(is.numAttributes() - 1);
+        }
+
+
+        for(int i=0;i<inputInstances.size();i++){
+
+            int positive = i;
+            Instances positiveInstance = inputInstances.get(i);
+
+            ArrayList<Instances> allInstances  = inputInstances;
+            //negativeInstance.remove(positive);
+            
+            //remove 4th columns
+            int[] indicesToRemove = {positiveInstance.numAttributes()-1};
+            Remove removeFilter = new Remove();
+            removeFilter.setAttributeIndicesArray(indicesToRemove);
+            removeFilter.setInvertSelection(false);
+        
+
+            ArrayList<Instances> removedClassInstance  = new ArrayList<>();
+            for(Instances is : allInstances){
+                removeFilter.setInputFormat(is);
+                Instances tmp = Filter.useFilter(is, removeFilter);
+                removedClassInstance.add(tmp);
+            }
+
+
+            //add new attribute class "NOMINAL"
+            Add filterADD = new Add();
+            filterADD.setAttributeIndex("last");
+            filterADD.setAttributeName("class");
+
+            ArrayList<Instances> addedClassInstance  = new ArrayList<>();
+            for(Instances is : removedClassInstance){
+                filterADD.setInputFormat(is);
+                Instances tmp = Filter.useFilter(is, filterADD);
+                if (tmp.classIndex() == -1) tmp.setClassIndex(tmp.numAttributes() - 1);
+                addedClassInstance.add(tmp);
+            }
+            
+            Instances positiveIs = addedClassInstance.remove(positive);
+
+
+            //set negative to all classes
+            for(Instances is : addedClassInstance){
+                for (int j = 0; j < is.numInstances(); j++) {
+                    // 2. nominal
+                    is.instance(j).setValue(is.numAttributes() - 1, 1);
+                }
+            }
+
+            //positive instance
+            for (int j = 0; j < positiveIs.numInstances(); j++) {
+                // 2. nominal
+                positiveIs.instance(j).setValue(positiveIs.numAttributes() - 1, 0);
+            }
+
+            //create aggregate Dataset
+
+            for(Instances its: addedClassInstance){
+                for(Instance is : its) positiveIs.add(is);
+            }
+
+            if (positiveIs.classIndex() == -1) positiveIs.setClassIndex(positiveIs.numAttributes() - 1);
+
+
+            //numeric to nominal
+            NumericToNominal numericToNominal = new NumericToNominal();
+            String[] options = new String[2];
+            options[0] = "-R";
+            options[1] = "last";
+            numericToNominal.setOptions(options);
+            numericToNominal.setInputFormat(positiveIs);
+
+
+            Instances nominalIs = Filter.useFilter(positiveIs, numericToNominal);
+            outputInstances.add(nominalIs);
+            
+            System.out.println("TOTAL inside records: "+positiveIs.numInstances());
+            System.out.println("Attribute number: "+positiveIs.numAttributes());
+            // File arfFIle = new File("src/main/java/org/resources/irisSingleCl/inputDS/agg_positive"+1+".csv");
+            // if(arfFIle.exists()) arfFIle.delete();
+            // ArffSaver rawtrainingSaver = new ArffSaver();
+            // rawtrainingSaver.setInstances(positiveIs);
+            // rawtrainingSaver.setFile(arfFIle);
+            // rawtrainingSaver.writeBatch();
+            
+            
+        } //end cycle
+
+
+        for (int i=0;i<outputInstances.size();i++){
+            File arfFIle = new File("src/main/java/org/resources/irisSingleCl/inputDS/agg_positive"+i+".arff");
+            if(arfFIle.exists()) arfFIle.delete();
+            ArffSaver rawtrainingSaver = new ArffSaver();
+            rawtrainingSaver.setInstances(outputInstances.get(i));
+            rawtrainingSaver.setFile(arfFIle);
+            rawtrainingSaver.writeBatch();
+        }
+
+
+        return;
+    }
+
     public static void main( String[] args ) throws Exception {
+
+
+        ArrayList<String> sources = new ArrayList<>();
+        sources.add("src/main/java/org/resources/irisSingleCl/1.csv");
+        sources.add("src/main/java/org/resources/irisSingleCl/2.csv");
+        sources.add("src/main/java/org/resources/irisSingleCl/3.csv");
+
+
+        FormatDataset(sources);
+
+
+        /*
         //DataSource source = new DataSource("/Users/thenor/Desktop/Aldo/Neodata/SaverioProjects/BinaryClassification/src/main/java/org/resources/irisSingleCl/1.csv");
         DataSource source = new DataSource("/Users/thenor/Desktop/Aldo/Neodata/SaverioProjects/BinaryClassification/src/main/java/org/resources/irisSingleCl/inputDS/positive1.csv");
+        DataSource source2 = new DataSource("/Users/thenor/Desktop/Aldo/Neodata/SaverioProjects/BinaryClassification/src/main/java/org/resources/irisSingleCl/inputDS/positive2.csv");
         Instances training = source.getDataSet();
+        Instances trainin2 = source2.getDataSet();
+
+        
+        for(Instance is: trainin2){
+            training.add(is);
+
+        }
+
         // header to fix https://stackoverflow.com/questions/3517186/using-weka-java-code-how-convert-csv-without-header-row-to-arff-format
 
         training.renameAttribute(0,"sepal length");
@@ -64,9 +218,7 @@ public class App
         trainingNegative_1.renameAttribute(4,"class");
         trainingNegative_2.renameAttribute(4,"class");
 
-         */
-
-        /*
+    
         int[] indicesToRemove = {4};
         Remove removeFilter = new Remove();
         removeFilter.setAttributeIndicesArray(indicesToRemove);
@@ -143,9 +295,16 @@ public class App
         //Instances allTraining = Instances.mergeInstances(tmp1,training);
 
 
-        //Remove percentage FROM TRAINING ALL
+
          */
 
+        //Remove percentage FROM TRAINING ALL
+
+        DataSource source = new DataSource("src/main/java/org/resources/irisSingleCl/inputDS/agg_positive0.arff");
+        Instances training = source.getDataSet();
+        if (training.classIndex() == -1) {
+            training.setClassIndex(training.numAttributes() - 1);
+        }
 
 
         RemovePercentage rp = new RemovePercentage();
@@ -180,7 +339,6 @@ public class App
         Evaluation eval = new Evaluation(training);
         eval.crossValidateModel(tree, testing, folds, new Random(1));
 
-
         //TPR, la precisione e la recall.
         //cross validation
         System.out.println(eval.toSummaryString("\nResults\n======\n", true));
@@ -188,32 +346,48 @@ public class App
         System.out.println("precision : "+eval.weightedPrecision());
         System.out.println("recall : "+eval.weightedRecall());
 
-        //multiclass j48
 
-        DataSource sourceAll = new DataSource("/Users/thenor/Desktop/Aldo/Neodata/SaverioProjects/BinaryClassification/src/main/java/org/resources/irisSingleCl/inputDS/positive1.csv");
-        Instances trainingAll= sourceAll.getDataSet();
+
+        //multiclass j48
+        
+        DataSource sourceBase = new DataSource("src/main/java/org/resources/irisSingleCl/inputDS/allClasses.csv");
+        
+        Instances sourceIs= sourceBase.getDataSet();
         // header to fix https://stackoverflow.com/questions/3517186/using-weka-java-code-how-convert-csv-without-header-row-to-arff-format
 
-        trainingAll.renameAttribute(0,"sepal length");
-        trainingAll.renameAttribute(1,"sepal width");
-        trainingAll.renameAttribute(2,"petal length");
-        trainingAll.renameAttribute(3,"petal width");
-        trainingAll.renameAttribute(4,"class");
+        sourceIs.renameAttribute(0,"sepal length");
+        sourceIs.renameAttribute(1,"sepal width");
+        sourceIs.renameAttribute(2,"petal length");
+        sourceIs.renameAttribute(3,"petal width");
+        sourceIs.renameAttribute(4,"class");
 
-        if (trainingAll.classIndex() == -1) trainingAll.setClassIndex(trainingAll.numAttributes() - 1);
+        if (sourceIs.classIndex() == -1) sourceIs.setClassIndex(sourceIs.numAttributes() - 1);
 
+        NumericToNominal numericToNominal = new NumericToNominal();
+        String[] optionsNumeric = new String[2];
+        optionsNumeric[0] = "-R";
+        optionsNumeric[1] = "last";
+        numericToNominal.setOptions(optionsNumeric);
+        numericToNominal.setInputFormat(sourceIs);
+
+
+        Instances nominalALL = Filter.useFilter(sourceIs, numericToNominal);
+        if (nominalALL.classIndex() == -1) nominalALL.setClassIndex(nominalALL.numAttributes() - 1);
+
+        
         //save arff
         File arfFile_all = new File("/Users/thenor/Desktop/Aldo/Neodata/SaverioProjects/BinaryClassification/src/main/java/org/resources/irisSingleCl/training_all.arff");
         if(arfFile_all.exists()) arfFile_all.delete();
         ArffSaver rawtrainingSaverAll = new ArffSaver();
-        rawtrainingSaverAll.setInstances(trainingAll);
+        rawtrainingSaverAll.setInstances(nominalALL);
         rawtrainingSaverAll.setFile(arfFile_all);
         rawtrainingSaverAll.writeBatch();
 
 
-        Instances testingAll = Filter.useFilter(trainingAll, rp);
+        rp.setInputFormat(nominalALL);
+        Instances testingAll = Filter.useFilter(nominalALL, rp);
 
-        Evaluation evalAll = new Evaluation(trainingAll);
+        Evaluation evalAll = new Evaluation(nominalALL);
         evalAll.crossValidateModel(tree, testingAll, folds, new Random(1));
 
         System.out.println(evalAll.toSummaryString("\nResults\n======\n", true));
